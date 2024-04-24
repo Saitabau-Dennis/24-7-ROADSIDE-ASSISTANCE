13b7d3c63fd62d307f60cb066b4e3bec538eba7e
KEEN <kennicmwimu@gmail.com>
2024-04-24 19:53:25 +0300

    maybe i cofigured firebase

--- roadsideassistant/lib/main.dart
+++ roadsideassistant/lib/main.dart
@@ -1,11 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:roadsideassistant/Splash_screen.dart';
 import 'package:roadsideassistant/home_page.dart';
 import 'login_screen.dart';
 import 'signup_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: "AIzaSyDEXrFv0zzG5qjgRpVGXSLP7sOXPRRbti4",
+          authDomain: "rdhelp-ce0bc.firebaseapp.com",
+          projectId: "rdhelp-ce0bc",
+          storageBucket: "rdhelp-ce0bc.appspot.com",
+          messagingSenderId: "178854411135",
+          appId: "1:178854411135:web:8663f2afb50a45ea09f84a"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
